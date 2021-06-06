29133fbb4d5c20908ed17910141599b2fab555cc
rateterin <rateterin@yandex.ru>
2021-06-06 22:49:56 +0300

    Add support MEDIA-files

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 import products.views as products
 
 urlpatterns = [
@@ -23,3 +25,6 @@ urlpatterns = [
     path('', products.home, name='home'),
     path('products/', products.products, name='products'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
