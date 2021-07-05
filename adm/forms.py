c4874378231e12c50c54158b9394e4f26539f0c0
rateterin <rateterin@yandex.ru>
2021-07-05 11:59:57 +0300

    Created AdmUserUpdate and AdmUserDelete mechanism of custom admin-panel Added rendering link to castom admin-panel for user.is_stuff Added rendering links to full and custom admin-panels for user.is_superuser

--- adm/forms.py
+++ adm/forms.py
@@ -1,36 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
-from authapp.forms import ShopUserProfileForm
-from authapp.models import ShopUser
+from authapp.forms import BaseRegisterForm, ShopUserProfileForm
 from django import forms
 
 
-class AdmUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите имя'}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите фамилию'}))
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}), required=True)
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите пароль'}), required=True, )
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Подтвердите пароль'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите адрес эл. почты'}), required=False)
-    avatar = forms.ImageField(widget=forms.FileInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Загрузите аватар'}), required=False)
-
-    class Meta:
-        model = ShopUser
-        fields = ('username', 'first_name', 'last_name', 'password1', 'password2', 'email', 'avatar')
-        exclude = ['age']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control py-4'
-            field.help_text = ''
+class AdmUserCreationForm(BaseRegisterForm):
+    pass
 
 
 class AdmUserUpdateForm(ShopUserProfileForm):
-    pass
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': False}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4', 'readonly': False}))
--- authapp/forms.py
+++ authapp/forms.py
@@ -1,8 +1,35 @@
 from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, UserCreationForm
-from adm.forms import AdmUserCreationForm
 from django import forms
 
 from .models import ShopUser
+
+
+class BaseRegisterForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите фамилию'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}), required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите пароль'}), required=True, )
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Подтвердите пароль'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите адрес эл. почты'}), required=False)
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Загрузите аватар'}), required=False)
+
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'last_name', 'password1', 'password2', 'email', 'avatar')
+        exclude = ('age',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+            field.help_text = ''
 
 
 class ShopUserLoginForm(AuthenticationForm):
@@ -21,7 +48,7 @@ class ShopUserLoginForm(AuthenticationForm):
             field.widget.attrs['class'] = 'form-control'
 
 
-class ShopUserRegisterForm(AdmUserCreationForm):
+class ShopUserRegisterForm(BaseRegisterForm):
     age = forms.IntegerField(widget=forms.NumberInput(attrs={
         'class': 'form-control py-4', 'placeholder': 'Введите Ваш возраст'}), required=True)
 
