be8d2f3ab8ba2f89825a91c4e1f4fb77c6a8f00d
rateterin <rateterin@yandex.ru>
2021-07-29 21:15:17 +0300

    Реализовано добавление товаров в заказ при помощи django-dynamic-formset

--- ordersapp/static/js/orders_scripts.js
+++ ordersapp/static/js/orders_scripts.js
@@ -37,16 +37,16 @@ window.onload = function () {
         }
     });
 
-    $('.order_form').on('click', 'input[type="checkbox"]', function (event) {
-        var target = event.target;
-        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
-        if (target.checked) {
-            delta_quantity = -quantity_arr[orderitem_num];
-        } else {
-            delta_quantity = quantity_arr[orderitem_num];
-        }
-        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
-    });
+    // $('.order_form').on('click', 'input[type="checkbox"]', function (event) {
+    //     var target = event.target;
+    //     orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
+    //     if (target.checked) {
+    //         delta_quantity = -quantity_arr[orderitem_num];
+    //     } else {
+    //         delta_quantity = quantity_arr[orderitem_num];
+    //     }
+    //     orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+    // });
 
     function orderSummaryUpdate(orderitem_price, delta_quantity) {
         delta_cost = orderitem_price * delta_quantity;
@@ -57,4 +57,24 @@ window.onload = function () {
         $('.order_total_cost').html(order_total_cost.toString());
         $('.order_total_quantity').html(order_total_quantity.toString());
     }
+
+    $('.formset_row').formset({
+        addText: 'добавить продукт',
+        deleteText: 'удалить',
+        prefix: 'orderitems',
+        removed: 'deleteOrderItem'
+    });
+
+    function deleteOrderItem(row) {
+        var target_name = row[0].querySelector('input[type="number"]').name;
+        orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
+        delta_quantity = -quantity_arr[orderitem_num];
+        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+    }
+
+    $('.order_form select').change(function (event) {
+        var target = event.target;
+        console.log(target);
+    });
+
 }
