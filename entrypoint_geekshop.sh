6ea56e8fce09b333470ca93772e778a017a494a3
rateterin <rateterin@yandex.ru>
2021-08-12 15:54:47 +0300

    Проект подготовлен к развертыванию командой docker-compose up

--- entrypoint_geekshop.sh
+++ entrypoint_geekshop.sh
@@ -7,8 +7,11 @@ pip install --no-cache-dir -r ./requirements.txt
 sleep 10
 
 python manage.py makemigrations
-python manage.py makemigrations deal
-python manage.py makemigrations customer
+#python manage.py makemigrations adm
+#python manage.py makemigrations authapp
+#python manage.py makemigrations baskets
+#python manage.py makemigrations ordersapp
+#python manage.py makemigrations products
 python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn --bind 0.0.0.0:8000 -w 3 testwebservice.wsgi:application
