7598c6c76ced09c78230d2904e221b229fe45743
rateterin <rateterin@yandex.ru>
2021-10-04 23:51:29 +0300

    vk integration fix; other little fixes.

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -45,4 +45,4 @@ else:
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls, namespace='djdt'))]
+    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
