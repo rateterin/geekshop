083030b8d8df43a6f775d72bfa916b228df19119
rateterin <rateterin@yandex.ru>
2022-10-18 19:35:16 +0300

    проверка 2

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -210,8 +210,7 @@ if DEBUG:
 
     DEBUG_TOOLBAR_PANELS = [
         'debug_toolbar.panels.versions.VersionsPanel', 'debug_toolbar.panels.timer.TimerPanel', 'debug_toolbar.panels.settings.SettingsPanel',
-        'debug_toolbar.panels.headers.HeadersPanel',
-        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.headers.HeadersPanel', 'debug_toolbar.panels.request.RequestPanel',
         'debug_toolbar.panels.sql.SQLPanel',
         'debug_toolbar.panels.templates.TemplatesPanel',
         'debug_toolbar.panels.staticfiles.StaticFilesPanel',
